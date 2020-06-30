ba741ef9abcc9aae9e2e2c28a935c3d411789efc
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-06-30 17:43:29 +0300

    update

--- New folder/books/src/App.js
+++ New folder/books/src/App.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   state={
-    books: []
+    books: [],
+  }
+
+  componentDidMount = async () => {
+    const apiUrl = process.env.REACT_APP_API_URL 
+    const resp = await fetch(`${apiUrl}/books`)
+    const jsonBooks = await resp.json()
+    
+
+    this.setState({
+      books:jsonBooks.data,
+    })
   }
 
   render(){
     return(
-      <div>
+      <div className ="App">
+        <header className ="App-header">
         <h1>Book catalogue</h1>
         {this.state.books.map((book, index) => (
           <div key ={index}>
-            <img src={book.img} style={{width: "200px"}}></img>
+            <img src={book.img} style={{width: "200px"}} alt="img"></img>
             <span>{book.title}</span>
             </div>
         ))}
+        </header>
       </div>
     )
   }
 
-  componentDidMount =async () => {
-    const apiUrl = process.env.REACT_APP_API_URL 
-    const resp = await fetch(`${apiUrl}/books`)
-    const jsonBooks = await resp.json()
-
-    this.setState({
-      books:jsonBooks.data
-    })
-  }
+ 
 }
 
 export default App;
