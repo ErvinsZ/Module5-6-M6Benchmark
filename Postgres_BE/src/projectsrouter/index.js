d95218d3087e48ca9c59d05161625d57bcc19ac8
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-07-16 15:13:25 +0300

    get projects by student id postgress

--- Postgres_BE/src/projectsrouter/index.js
+++ Postgres_BE/src/projectsrouter/index.js
@@ -3,7 +3,15 @@ const db = require("../db")
 
 const router = express.Router()
 
+router.get("/:id", async (req, res)=>{
+    const response = await db.query('SELECT * FROM projects WHERE studentid = $1', 
+                                                                                        [ req.params.id ])
 
+    if (response.rowCount === 0) 
+        return res.status(404).send("Not found")
+
+    res.send(response.rows)
+})
 
 
 
